07f08fee6c1537264ef2714174eba00af3d8eb2e
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-30 19:52:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
+  // Change the response to render the Pug template
   res.render('index');
 });
 

65991fcceb72fe99aba276861a46b098619dd8d5
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-30 17:55:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.route('/').get((req, res) => {
-  res.render(index);
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;

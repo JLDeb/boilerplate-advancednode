29edecf8c1fd6e14c01ab9532385cb5ece8d5329
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-30 17:37:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+res.render(index);
 
 app.route('/').get((req, res) => {
 

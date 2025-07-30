d53cdab729bddc3d216056f37cec1de5c26844d0
JLDeb <99515799+JLDeb@users.noreply.github.com>
2025-07-30 17:51:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+res.render(index);
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
-res.render(index);
+
 
 app.route('/').get((req, res) => {
 
